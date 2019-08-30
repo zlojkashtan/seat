6c86e02629566b3aedf3f74934d5ba3982f2724a
Yurii Skoriiantov <zlojkashtan@gmail.com>
2019-08-30 16:32:14 +0300

    16:32

--- send.php
+++ send.php
@@ -9,9 +9,13 @@ require './src/Exception.php';
 
 if(isset($_POST['f_auto'], $_POST['f_dealer'], $_POST['f_name'], $_POST['f_phone'])){
 
+  $string = file_get_contents("./data.json");
+  $json_a = json_decode($string, true);
+
   $fields = array(
     'auto'=>$_POST['f_auto'],
     'dealer'=>$_POST['f_dealer'],
+    'dealerMail'=>$json_a[$_POST['f_dealer']],
     'name'=>$_POST['f_name'],
     'phone'=> $_POST['f_phone']
     );
@@ -26,14 +30,14 @@ if(isset($_POST['f_auto'], $_POST['f_dealer'], $_POST['f_name'], $_POST['f_phone
   $mail->SMTPSecure = 'tls';                        // Enable TLS encryption, `ssl` also accepted
   $mail->Port       = 465;
 
-  $mail->setFrom('from@example.com', 'Mailer');         // Отправитель
-  $mail->addReplyTo('info@example.com', 'Information'); // Ответ
-  $mail->addAddress('zlojkashtan@gmail.com', 'Joe User');     // Получатель
+  $mail->setFrom('from@example.com', 'Mailer');             // Отправитель
+  $mail->addReplyTo('info@example.com', 'Information');     // Ответ
+  $mail->addAddress('zlojkashtan@gmail.com', 'Joe User');   // Получатель
+  $mail->addCC($fields['dealerMail']);                      // Копия диллеру
 
   $mail->isHTML();
   $mail->Subject = 'Заявка на тестдрайв';
   $mail->Body = '<p>' . $fields['auto'] . '</p>'.'<p>' . $fields['deler'] . '</p>'.'<p>' . $fields['name'] . '</p>'.'<p>' . $fields['phone'] . '</p>';
-
 
   if ($mail->send()) {
     echo "ok";
